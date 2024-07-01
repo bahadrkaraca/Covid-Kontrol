ced9e74c8a8c2461b9bf32c9788354654d4a6f95
BahadrK <62482313+Bahadr6161@users.noreply.github.com>
2024-07-01 18:05:27 +0300

    Initial commit

---
+++ başla.php
@@ -0,0 +1,8 @@
+<?php echo "Merhaba". " ".
+foreach($_POST['er'] as $değer) {
+		echo $değer;. " ".
+	} 
+	$_POST['ad']." ".$_POST['sad']. " ".;
+	foreach($_POST['y'] as $değer) {
+		echo $değer;
+	}?>
