6e06bbd569e053da43ee6b83726e3b8b71e6fedc
BahadrK <62482313+Bahadr6161@users.noreply.github.com>
2024-07-02 21:15:46 +0300

    Update başla.php

--- başla.php
+++ başla.php
@@ -1,8 +1,11 @@
 <?php echo "Merhaba". " ".
-foreach($_POST['er'] as $değer) {
+	foreach($_POST['er'] as $değer) 
+	{
 		echo $değer;. " ".
 	} 
 	$_POST['ad']." ".$_POST['sad']. " ".;
-	foreach($_POST['y'] as $değer) {
+	foreach($_POST['y'] as $değer) 
+	{
 		echo $değer;
-	}?>
+	}
+?>
